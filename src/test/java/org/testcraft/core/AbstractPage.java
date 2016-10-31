70218cb12c9571dd635661ba5d2a189d43cc1031
Craig Duncan <craig@CraigMBPR.local>
2016-10-31 14:23:23 +0000

    Formatting and tidyup

--- src/test/java/org/testcraft/core/AbstractPage.java
+++ src/test/java/org/testcraft/core/AbstractPage.java
@@ -3,10 +3,9 @@ package org.testcraft.core;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class AbstractPage {
+  protected RemoteWebDriver driver;
 
-    protected RemoteWebDriver driver;
-
-    public AbstractPage(RemoteWebDriver remoteWebDriver){
+  public AbstractPage(RemoteWebDriver remoteWebDriver){
         this.driver = remoteWebDriver;
     }
 }
